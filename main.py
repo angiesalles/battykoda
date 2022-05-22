e5b200466c93209f8b7e98de5915d03d4a028ab6
Kevin Boergens <kboergens@paradromics.com>
2022-05-22 15:51:16 -0500

    some improvement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
+import tempfile
 import tables
 tables.file._open_files.close_all()
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, Markup, send_from_directory
 from PIL import Image
 import numpy as np
 import random
@@ -14,11 +16,13 @@ matplotlib.use('Agg')#https://stackoverflow.com/questions/2801882/generating-a-p
 import matplotlib.pyplot as plt
 import scipy.signal
 
-app = Flask(__name__)
+threshold = 0.01
 
+app = Flask(__name__)
+osfolder = 'C:/Users/Kevin/Documents/repos/capybara/data/'
 global_limit_confidence = 100
 global_user_name = ""
-global_myfile=h5py.File('/Users/angelessalles/Documents/GitHub/battykoda/templates/Users/Angie/Experiment/Date/Pair_Trial1_1638.mat', 'r')
+lookup = dict()
 def store_task(result):
     app.logger.info(result['type_call'])
     app.logger.info('stored!')
@@ -48,11 +52,36 @@ def get_task(limit_confidence):
 @app.route('/result', methods = ['POST','GET'])
 
 
-@app.route('/static/<path:path>')
+@app.route('/battykoda/<path:path>')
 def static_cont(path):
-    return send_from_directory('static', path)
+    ch2use = 0
+    if path[-5:] == '.mat/':
+        datafile = h5py.File(osfolder + path[:-1])
+        listims = []
+        for idx in range(3):
+            tf = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
+            plt.figure(figsize=(3,3))
+            plt.plot(datafile['ni_data']['mic_data'][ch2use, 0+idx*1000:1000+idx*1000])
+            plt.hlines(threshold,0,1000,'k')
+            plt.savefig(tf)
+            shorty = tf.name.split('\\')[-1]
+            lookup[shorty] = tf.name
+            listims.append('/battykoda/img/'+shorty)
+            tf.close()
+        return render_template('setThreshold.html',
+                               data={'images':listims,
+                                     'threshold':str(threshold)})
+    if path[:4] == 'img/':
+        return send_from_directory('/'.join(lookup[path[4:]].split('\\')[:-1]), path[4:])
 
-@app.route('/', methods = ['POST','GET'])
+    return render_template('listBK.html',
+                           data={'listicle':Markup(''.join(['<li><a href="'+item+'/">'+item+'</a></li>' for item in os.listdir((osfolder+path))]))})
+
+@app.route('/')
+def mainpage():
+    return render_template('welcometoBK.html', data=dict())
+
+@app.route('/task', methods = ['POST','GET'])
 def index():
     global global_user_name
     global global_limit_confidence
