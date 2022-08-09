90aad995b146992ef21190a125d11c4128dab786
Kevin Boergens <kboergens@paradromics.com>
2022-08-09 16:31:20 -0500

    DataReader provides hashes

--- DataReader.py
+++ DataReader.py
@@ -2,6 +2,7 @@ import h5py
 import numpy as np
 from scipy.io import wavfile
 import time
+import hashlib
 
 class DataReader:
     cache = dict()
@@ -11,7 +12,9 @@ class DataReader:
         if path_to_file in cls.cache and time.time() - cls.cache[path_to_file]['time'] < 300:
                 fs = cls.cache[path_to_file]['fs']
                 audiodata = cls.cache[path_to_file]['audiodata']
+                hashof = cls.cache[path_to_file]['hashof']
         else:
+            hashof = hashlib.md5(open(path_to_file, 'rb').read()).hexdigest()
             if path_to_file.endswith('.mat'):
                 datafile = h5py.File(path_to_file)
                 audiodata = np.array(datafile['ni_data']['mic_data'])
@@ -20,14 +23,15 @@ class DataReader:
                 fs, audiodata = wavfile.read(path_to_file)
             cls.cache[path_to_file] = {'time': time.time(),
                                        'fs': fs,
-                                       'audiodata': audiodata}
+                                       'audiodata': audiodata,
+                                       'hashof': hashof}
 
 
         assert (np.sum(np.array(audiodata.shape) > 1) == 1) #here is where we assert only one channel
         audiodata = audiodata.flatten().astype(float)
         audiodata /= np.std(audiodata)
 
-        return audiodata, fs
+        return audiodata, fs, hashof
 
 
 
