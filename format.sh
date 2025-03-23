b8eb239022d73ff7aa75bcc3342977dc1948ffde
Kevin Boergens <boergens@uic.edu>
2025-03-23 21:36:36 +0000

    linters

---
+++ format.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Colors for better output
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+echo -e "${YELLOW}Running isort to sort imports...${NC}"
+docker-compose exec -T web isort .
+
+echo -e "${YELLOW}Running black to format code...${NC}"
+docker-compose exec -T web black .
+
+echo -e "${GREEN}Code formatting complete!${NC}"
+echo -e "${YELLOW}Note: flake8 issues must be fixed manually. Run './lint.sh' to check for remaining issues.${NC}"
